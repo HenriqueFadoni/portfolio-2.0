58c86ffa11efe44ec31b65d4fe26a95d7d5a474f
Henrique Fadoni <henriquefadonii@gmail.com>
2019-04-04 09:50:07 -0300

    Set Up and Introduction page

---
+++ src/Pages/Introduction/Introduction.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Introduction = () => (
+    <section className="introduction">
+        <div className="introduction__grid">
+            <h1 className="title__big-blue">
+                <span>Hello</span>,
+            </h1>
+            <h1 className="title__medium-white">
+                My name is Henrique, I'm a Web Developer
+            </h1>
+        </div>
+    </section>
+);
+
+export default Introduction
