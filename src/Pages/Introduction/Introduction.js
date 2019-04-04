2b75edf7ad22be672eb2eeab1a60a17e02222968
Henrique Fadoni <henriquefadonii@gmail.com>
2019-04-04 12:04:54 -0300

    Creating Title for Introduction

--- src/Pages/Introduction/Introduction.js
+++ src/Pages/Introduction/Introduction.js
@@ -12,12 +12,12 @@ const Introduction = () => (
             Your browser is not supported!
         </video>
         </div>
-        <div className="introduction__grid">
+        <div className="introduction__grid__container">
             <h1 className="title__big-blue">
                 <span>Hello</span>,
             </h1>
-            <h1 className="title__medium-white">
-                My name is Henrique, I'm a Web Developer
+            <h1 className="margin-left-medium title__medium-blue">
+                My name is <span>Henrique</span>, I'm a <span>Web Developer</span>
             </h1>
         </div>
     </section>
