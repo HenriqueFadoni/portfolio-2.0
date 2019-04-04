983d0a7c5b1eb81495e1bf8d223b5cc710107191
Henrique Fadoni <henriquefadonii@gmail.com>
2019-04-04 11:18:21 -0300

    Creatiating INTRODUCTION background

--- src/Pages/Introduction/Introduction.js
+++ src/Pages/Introduction/Introduction.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import videoMP4 from '../../img/Love-Coding.mp4'
+import videoWEBM from '../../img/Love-Coding.webm'
+
 const Introduction = () => (
     <section className="introduction">
+        <div className="bg-video">
+        <video className="bg-video__content" autoPlay muted loop>
+            <source src={videoMP4} type="video/mp4"/>
+            <source src={videoWEBM} type="video/webm"/>
+            Your browser is not supported!
+        </video>
+        </div>
         <div className="introduction__grid">
             <h1 className="title__big-blue">
                 <span>Hello</span>,
