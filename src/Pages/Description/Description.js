63a6914a34a90e76ea62d3f0834f4b93b26cb2d4
Henrique Fadoni <henriquefadonii@gmail.com>
2019-04-05 19:57:49 -0300

    Basic Templates for Cards - DeDescription

--- src/Pages/Description/Description.js
+++ src/Pages/Description/Description.js
@@ -2,13 +2,28 @@ import React from 'react';
 
 const Description = () => (
     <section className="description">
-        <div className="description__cards-container">
-            <div className="description__cards">a</div>
-            <div className="description__cards">b</div>
-            <div className="description__cards">c</div>
+        <div className="description__cards">
+            <div className="description__cards-1">a</div>
+            <div className="description__cards-2">b</div>
+            <div className="description__cards-3">c</div>
         </div>
 
+        <div className="description__about"> 
+            <div className="description__about-text">
+                <p>Description text</p>
+            </div>
 
+            <div className="description__about-img">
+                <img src="" alt="My Image Profile"/>
+            </div>
+
+            <div className="description__about-skills">
+                <h2>My Skills</h2>
+                <ul>
+                    <li>JavaScript</li>
+                </ul>
+            </div>
+        </div>
     </section>
 );
 
