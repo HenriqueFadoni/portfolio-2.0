b7ae2c0e496d4f036e5dce383a2c0200eac3ed3c
Henrique Fadoni <henriquefadonii@gmail.com>
2019-04-06 17:01:32 -0300

    Creating Cards - Description

--- src/Pages/Description/Description.js
+++ src/Pages/Description/Description.js
@@ -1,20 +1,37 @@
 import React from 'react';
 
+import icon from '../../img/symbol-defs.svg';
+
 const Description = () => (
     <section className="description">
         <div className="description__cards">
-            <div className="description__cards-1">a</div>
-            <div className="description__cards-2">b</div>
-            <div className="description__cards-3">c</div>
+            <div className="description__cards-1">
+                <svg className="cards__icon margin-bottom-small margin-top-small">
+                    <use xlinkHref={`${icon}#icon-code`}></use>
+                </svg>
+                <p>I love coding Dynamic Websites and maniputaling data.</p>
+            </div>
+            <div className="description__cards-2">
+                <svg className="cards__icon margin-bottom-small margin-top-small">
+                    <use xlinkHref={`${icon}#icon-clock`}></use>
+                </svg>
+                <p>Nothing better than Fast and reliable Web Applications!</p>
+            </div>
+            <div className="description__cards-3">
+                <svg className="cards__icon margin-bottom-small margin-top-small">
+                    <use xlinkHref={`${icon}#icon-tv`}></use>
+                </svg>
+                <p>Responsive design is one of my major goals.</p>
+            </div>
         </div>
 
-        <div className="description__about"> 
+        <div className="description__about">
             <div className="description__about-text">
                 <p>Description text</p>
             </div>
 
             <div className="description__about-img">
-                <img src="" alt="My Image Profile"/>
+                <img src="" alt="My Image Profile" />
             </div>
 
             <div className="description__about-skills">
@@ -24,7 +41,7 @@ const Description = () => (
                 </ul>
             </div>
         </div>
-    </section>
+    </section >
 );
 
 export default Description;
