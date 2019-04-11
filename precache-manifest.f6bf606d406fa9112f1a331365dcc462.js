dafae76d83742eab8e81a8ce32a3277fef52db06
Henrique Fadoni <henriquefadonii@gmail.com>
2019-04-11 16:11:41 -0300

    Updates

---
+++ precache-manifest.f6bf606d406fa9112f1a331365dcc462.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5683999a71bbd66f022522acb9f56cca",
+    "url": "/portfolio/static/media/Love-Coding.5683999a.mp4"
+  },
+  {
+    "revision": "59e9d54602b21fcba82f",
+    "url": "/portfolio/static/css/main.aea36492.chunk.css"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "1b8b9099a8f3e3cedd0f",
+    "url": "/portfolio/static/js/2.f99a305e.chunk.js"
+  },
+  {
+    "revision": "d27829f0eed8801a9e28a6b33a7a4f96",
+    "url": "/portfolio/static/media/symbol-defs.d27829f0.svg"
+  },
+  {
+    "revision": "59e9d54602b21fcba82f",
+    "url": "/portfolio/static/js/main.272d34d2.chunk.js"
+  },
+  {
+    "revision": "2574f75ca44b0010e0c701b0656e7dca",
+    "url": "/portfolio/static/media/Love-Coding.2574f75c.webm"
+  },
+  {
+    "revision": "416d1f0fa3a09e351fca41bad62c1ece",
+    "url": "/portfolio/static/media/image-4.416d1f0f.jpg"
+  },
+  {
+    "revision": "c40140880c6a655ace54b053a3db5feb",
+    "url": "/portfolio/static/media/image-2.c4014088.jpg"
+  },
+  {
+    "revision": "04006883bdfba09af7f650bdd9c8b6e8",
+    "url": "/portfolio/static/media/image-5.04006883.jpg"
+  },
+  {
+    "revision": "767e8f8f4db20f6851535e1de9a65bf3",
+    "url": "/portfolio/index.html"
+  }
+];
